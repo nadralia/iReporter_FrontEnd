4d45024cfecf66f93a41266f80aa27f69ed1bebd
nadralia <nadralia@gmail.com>
2019-06-03 15:54:41 +0300

    chore(Setup) Testing/eslint environment add .eslintrc.js file install enzyme,jest,enzyme-adapter-react-16 add webpack.common.js add webpack.dev.js add webpack.prod.js
    [maintains chore]

---
+++ webpack.common.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    filename: 'index.bundle.js',
+    path: path.resolve('./dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: ['/node_modules'],
+        use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.s?css/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(gif|png|jpe?g|woff|woff2|eot|ttf|svg)$/i,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+         test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+         use: "file-loader?limit=10000&mimetype=application/octet-stream"
+       }, {
+         test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+         use: "file-loader"
+       },
+    ]
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    extensions: ['*', '.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+      title: "production"
+    })
+  ]
+}
