8c8c9bf0acbb9cb848804e3e53ab1b35e4a8bed2
nadralia <nadralia@gmail.com>
2019-06-24 15:13:04 +0300

    ch(Routes):setup react routes add react-router-dom add login and register components changed the webpack config

---
+++ src/components/Auth/Login.js
@@ -0,0 +1,14 @@
+// import react libraries
+import React, { Component } from 'react';
+
+class Login extends Component {
+    render() {
+        return (
+            <div>
+                <h1>login form</h1> 
+            </div>
+        )
+    }
+}
+
+export default Login;
---
+++ src/components/Auth/Register.js
@@ -0,0 +1,14 @@
+// import react libraries
+import React, { Component } from 'react';
+
+class Register extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Register form</h1> 
+            </div>
+        )
+    }
+}
+
+export default Register;
