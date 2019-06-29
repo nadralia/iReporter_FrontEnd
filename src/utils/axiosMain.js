1f3cc8ca2f9bc7c1137f06b2089b5b3718b9a54e
nadralia <nadralia@gmail.com>
2019-06-29 20:07:07 +0300

    Add signup and login functionality for the ireporter

--- src/utils/axiosMain.js
+++ src/utils/axiosMain.js
@@ -2,11 +2,11 @@
 import axios from 'axios';
 
 // pick token from sessions
-const token = sessionStorage.getItem('token');
+const token = sessionStorage.getItem('Token');
 
 // create an instance of axios
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:5000/api/v2/',
+  baseURL: 'https://dbireporter.herokuapp.com/api/v2/',
   headers: {
     Authorization: `Bearer ${token}`,
   },
